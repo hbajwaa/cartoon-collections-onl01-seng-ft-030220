a9449f7d30d88e08483e8c5377f17bdf8402cff1
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 23:37:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,10 @@ def summon_captain_planet(names)
   end
 end
 
-def long_planeteer_calls
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
